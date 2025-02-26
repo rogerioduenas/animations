62c9f16e4a11efaa056617f0b858a462ca0a913f
rogerioduenas <rogerioduenasdev@hotmail.com>
2025-02-26 15:24:15 +0900

    feat: loading

--- src/components/Animations/Animation-1/index.jsx
+++ src/components/Animations/Animation-1/index.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import styles from './style.module.scss';
 
 function Animation1() {
   return (
-    <div>Animation1</div>
+    <Box className={styles.body}>
+      <Box className={styles.loader}>
+        <svg viewBox='0 0 200 200'>
+          <circle cx='100' cy='100' r='50'></circle>
+        </svg>
+      </Box>
+    </Box>
   )
 }
 
