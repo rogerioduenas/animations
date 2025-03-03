023928b67da04848f0a67d7db02ac3f1f96169d3
rogerioduenas <rogerioduenasdev@hotmail.com>
2025-03-03 14:44:33 +0900

    feat: checkbox

--- src/components/Animations/Animation-1/index.jsx
+++ src/components/Animations/Animation-1/index.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
 import styles from './style.module.scss';
 
 function Animation1() {
   return (
-    <Box className={styles.body}>
-      <Box className={styles.loader}>
+    <div className={styles.body}>
+      <div className={styles.loader}>
         <svg viewBox='0 0 200 200'>
           <circle cx='100' cy='100' r='50'></circle>
         </svg>
-      </Box>
-    </Box>
+      </div>
+    </div>
   )
 }
 
--- src/components/Animations/Animation-2/index.jsx
+++ src/components/Animations/Animation-2/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Box, Typography } from '@mui/material';
+import styles from './style.module.scss';
 
 function Animation2() {
   return (
-    <div>Animation2</div>
+    <div className={styles.body}>
+      <div className={styles.container}>
+        <p>Do you like this ?</p>
+        <label>
+          <input type='radio' name='btn' />
+          <span className={styles.check}></span>
+          Yes
+        </label>
+        <label>
+          <input type='radio' name='btn' />
+          <span className={styles.cross}></span>
+          No
+        </label>
+      </div>
+    </div>
   )
 }
 
